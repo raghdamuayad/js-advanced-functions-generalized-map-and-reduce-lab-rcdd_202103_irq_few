d70215e895d67c9ff82e370b92eb1f2433e0a6f3
Raghda  Muayad <therose000777@gmail.com>
2021-05-13 21:47:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Add your functions here
-function map(arr, callback) {
+function map(arr, callArray) {
 let result = [];
 for (let i = 0; i < arr.length; i++) {
-result.push(callback(arr[i]));
+result.push(callArray(arr[i]));
 }
 return result;
 }
-function reduce(arr, callback, start) {
+function reduce(arr, callArray, start) {
 let accArray = start ? start : arr[0];
 let i = start ? 0 : 1;
 
 for (; i < arr.length; i++) {
-accArray = callback(arr[i], accArray);
+accArray = callArray(arr[i], accArray);
 }
 return accArray;
 }
