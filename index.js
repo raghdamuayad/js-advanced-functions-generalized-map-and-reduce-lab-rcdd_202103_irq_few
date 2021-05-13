83e43f7a976064fbf7cc142de4a9ec827395c6dc
Raghda  Muayad <therose000777@gmail.com>
2021-05-13 21:32:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Add your functions here
-function map(arr,func){
-  const newArr = []
+function map(array,func){
+  const newArray = []
   for (let i = 0; i < arr.length; i++) {
-    newArr.Push(func(arr[i]))
+    newArray.Push(func(array[i]))
   }
+  return newArray
+}
+function reduce(array,func,start = 0){
+  let gm = !! start ? :array[0]
+  let i = !! startc ? 0 :1
+  for (var i = 0; i < array.length; i++) {
+  gm = func ( array[i], gm)
+  }
+  return gm
 }
