e0ce24d7cd1effeb096025feead509bacf30e0af
Raghda  Muayad <therose000777@gmail.com>
2021-05-13 21:49:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(arr, callArray) {
+let result = [];
+for (let i = 0; i < arr.length; i++) {
+result.push(callArray(arr[i]));
+}
+return result;
+}
+function reduce(arr, callArray, start) {
+let accArray = start ? start : arr[0];
+let i = start ? 0 : 1;
+
+for (; i < arr.length; i++) {
+accArray = callArray(arr[i], accArray);
+}
+return accArray;
+}
